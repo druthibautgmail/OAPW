34739fa35261e1b6897af2dfee219c4fab0fe227
Dr. Ulrich Thibaut <ulrich.thibaut@gmail.com>
2026-07-16 17:23:33 +0200

    Implement DelayEngine::prepare

--- src/delay_engine.cpp
+++ src/delay_engine.cpp
@@ -9,6 +9,15 @@ DelayEngine::DelayEngine() = default;
 void DelayEngine::prepare(double sampleRate,
                           std::size_t maximumDelaySamples)
 {
+    sampleRate_ = sampleRate;
+
+    maximumDelay_ = maximumDelaySamples;
+
+    delaySamples_ = 0.0;
+
+    writeIndex_ = 0;
+
+    buffer_.assign(maximumDelay_ + 1, 0.0f);
 }
 
 void DelayEngine::reset()
